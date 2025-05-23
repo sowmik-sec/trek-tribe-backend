cabbf6c547def0009e86956b38b41bee9127626a
sowmik-sec <sowmik.sec@gmail.com>
2025-05-23 22:21:00 +0600

    add environment variable to config file

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+const envPath = path.join(process.cwd(), '.env');
+dotenv.config({ path: envPath });
+
+export default {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  cloudinary: {
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  },
+};
